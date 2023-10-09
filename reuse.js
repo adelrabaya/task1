e710fb5760fa8f43a00fbeb498d4aed6eab271a5
adelhajsaleh <adel.rabaya@gmail.com>
2023-10-09 08:58:37 +0300

    Version.1 (Done?)

--- reuse.js
+++ reuse.js
@@ -5,16 +5,18 @@
       this.innerHTML = 
         `
         <div class="row g-0 align-items-center">
-        <div class="col-lg-8 col-12 order-first order-md-first  ">
-            <div class="w-100">
+        <div class="col-lg-8 col-12 order-first order-md-first ">
+            <div class="w-100" style="display:flex;flex-direction:column;text-align:right;">
+                <h1 class="service"style="font-weight:bold;font-size:3rem;color:black;"> Space as a Service </h1>
                 <img src="https://www.vari.com/dw/image/v2/BDFT_PRD/on/demandware.static/-/Library-Sites-VariGlobalContentLibrary/default/dwaff338fb/homepage-b2c/desktop/vari-hp-b2c-module-hero-1-400817-c2.jpg?sw=1500" class="img-fluid"  alt="office picture">
             </div>
         </div>
-        <div class="col-lg-4 col-12 order-last order-md-last">
-            <aside class="justify-content-center w-85 m-5">
-                <h3 class="display-6  fw-bold">Quality We Can Stand Behind</h3>
+        <div class="col-lg-4 col-12 order-last order-md-last d-flex flex-column justify-content-center">
+            <aside class="  w-85 m-3 p-3 my-header">
+                <h3 class="display-6  fw-bold">VariSpace® Southlake is Move-In<br> Ready</h3>
                 <p>Our VariDesk line of desktop converters set the industry standard and inspired plenty of other products on the market, but there's a reason we continue to be the #1 name in sit-stand desk solutions: Unmatched quality and customer service.
                 <br><br>Every Vari product is tested and certified to meet the highest industry standards for durability and safety, and each one comes with a solid warranty and a risk-free, 30-Day Hassle-Free Returns Policy.</p>
+                <button type="button" class="btn  site-btns mt-3 col-5 w-75">LEARN MORE ABOUT VARISPACE</button>
             </aside>
 
         </div>
